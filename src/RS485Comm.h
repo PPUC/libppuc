0b5f7424dac08896fa9c8290d825cb14dba61e84
Markus Kalkbrenner <git@kalki.de>
2024-01-06 14:40:22 +0100

    coding style

--- src/RS485Comm.h
+++ src/RS485Comm.h
@@ -1,12 +1,12 @@
 #pragma once
 
-#include <thread>
-#include <queue>
-#include <string>
 #include <inttypes.h>
 #include <mutex>
-#include <stdio.h>
+#include <queue>
 #include <stdarg.h>
+#include <stdio.h>
+#include <string>
+#include <thread>
 #include "PPUC.h"
 #include "io-boards/Event.h"
 #include "libserialport.h"
@@ -17,11 +17,11 @@
 #define CALLBACK
 #endif
 
-#define RS485_COMM_BAUD_RATE 115200
-#define RS485_COMM_SERIAL_READ_TIMEOUT 8
+#define RS485_COMM_BAUD_RATE            115200
+#define RS485_COMM_SERIAL_READ_TIMEOUT  8
 #define RS485_COMM_SERIAL_WRITE_TIMEOUT 8
 
-#define RS485_COMM_MAX_BOARDS 16
+#define RS485_COMM_MAX_BOARDS           16
 
 #if _MSC_VER
 #define RS485_COMM_MAX_SERIAL_WRITE_AT_ONCE 256
@@ -33,54 +33,53 @@
 
 #define RS485_COMM_QUEUE_SIZE_MAX 128
 
-typedef void(CALLBACK *PPUC_LogMessageCallback)(const char *format, va_list args, const void *userData);
+typedef void(CALLBACK* PPUC_LogMessageCallback)(const char* format, va_list args, const void* userData);
 
-class RS485Comm
-{
-public:
-   RS485Comm();
-   ~RS485Comm();
+class RS485Comm {
+  public:
+    RS485Comm();
+    ~RS485Comm();
 
-   void SetLogMessageCallback(PPUC_LogMessageCallback callback, const void *userData);
+    void SetLogMessageCallback(PPUC_LogMessageCallback callback, const void* userData);
 
-   bool Connect(const char *device);
-   void Disconnect();
+    bool Connect(const char* device);
+    void Disconnect();
 
-   void Run();
+    void Run();
 
-   void QueueEvent(Event *event);
-   bool SendConfigEvent(ConfigEvent *configEvent);
+    void QueueEvent(Event* event);
+    bool SendConfigEvent(ConfigEvent* configEvent);
 
-   void RegisterSwitchBoard(uint8_t number);
-   PPUCSwitchState *GetNextSwitchState();
+    void RegisterSwitchBoard(uint8_t number);
+    PPUCSwitchState* GetNextSwitchState();
 
-   void SetDebug(bool debug);
+    void SetDebug(bool debug);
 
-private:
-   void LogMessage(const char *format, ...);
+  private:
+    void LogMessage(const char* format, ...);
 
-   bool SendEvent(Event *event);
-   Event *receiveEvent();
-   void PollEvents(int board);
+    bool SendEvent(Event* event);
+    Event* receiveEvent();
+    void PollEvents(int board);
 
-   PPUC_LogMessageCallback m_logMessageCallback = nullptr;
-   const void *m_logMessageUserData = nullptr;
+    PPUC_LogMessageCallback m_logMessageCallback = nullptr;
+    const void* m_logMessageUserData = nullptr;
 
-   uint8_t m_switchBoards[RS485_COMM_MAX_BOARDS];
-   uint8_t m_switchBoardCounter = 0;
-   bool m_activeBoards[RS485_COMM_MAX_BOARDS] = { false };
+    uint8_t m_switchBoards[RS485_COMM_MAX_BOARDS];
+    uint8_t m_switchBoardCounter = 0;
+    bool m_activeBoards[RS485_COMM_MAX_BOARDS] = {false};
 
-   bool m_debug = false;
+    bool m_debug = false;
 
-   // Event message buffers, we need two independent for events and config events because of threading.
-   uint8_t m_msg[7];
-   uint8_t m_cmsg[12];
+    // Event message buffers, we need two independent for events and config events because of threading.
+    uint8_t m_msg[7];
+    uint8_t m_cmsg[12];
 
-   struct sp_port *m_pSerialPort;
-   struct sp_port_config *m_pSerialPortConfig;
-   std::thread *m_pThread;
-   std::queue<Event *> m_events;
-   std::queue<PPUCSwitchState *> m_switches;
-   std::mutex m_eventQueueMutex;
-   std::mutex m_switchesQueueMutex;
+    struct sp_port* m_pSerialPort;
+    struct sp_port_config* m_pSerialPortConfig;
+    std::thread* m_pThread;
+    std::queue<Event*> m_events;
+    std::queue<PPUCSwitchState*> m_switches;
+    std::mutex m_eventQueueMutex;
+    std::mutex m_switchesQueueMutex;
 };
--- src/test.cpp
+++ src/test.cpp
@@ -1,19 +1,19 @@
 #include "PPUC.h"
 
-#include <stdarg.h>
 #include <chrono>
+#include <stdarg.h>
 #include <thread>
 
-void CALLBACK OnLogMessage(const char* format, va_list args, const void* pUserData)
-{
-   char buffer[1024];
-   vsnprintf(buffer, sizeof(buffer), format, args);
+void CALLBACK
+OnLogMessage(const char* format, va_list args, const void* pUserData) {
+    char buffer[1024];
+    vsnprintf(buffer, sizeof(buffer), format, args);
 
-   printf("%s\n", buffer);
+    printf("%s\n", buffer);
 }
 
-int main(int argc, const char *argv[])
-{
-   PPUC* pPPUC = new PPUC();
-   pPPUC->SetLogMessageCallback(OnLogMessage, NULL);
+int
+main(int argc, const char* argv[]) {
+    PPUC* pPPUC = new PPUC();
+    pPPUC->SetLogMessageCallback(OnLogMessage, NULL);
 }
